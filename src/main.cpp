9b20b4f414ac1f00eaf69ca48935f63f8d00f3c2
3lv <cristian.vlad.vladutu@gmail.com>
2023-08-21 16:38:19 +0300

    More input options

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,11 @@ const vector<string> MODES({"Insert", "Normal", "Normal.window", "Normal.square"
 
 int main() {
 	Dict dict;
-	Dict::mode = "Normal";
+	//Dict::mode = "Normal";
+	cout << "Possible modes are:\n";
+	for(auto m:MODES) cout << m << " ";
+	cout << endl;
+	getline(cin, Dict::mode);
 	bool running = true;
 	while(running) {
 		string input;
@@ -15,14 +19,15 @@ int main() {
 		cout << rec.command;
 		getline(cin, input);
 		cout << rec.action << endl;
-		cout << "Did you know what it does? [Y(es)/n(o)/a(bsolutely)] ";
+		cout << "Did you know what it does? [Y(es)/n(o)/a(bsolutely)/s(omehow?)] ";
 		getline(cin, input);
 		if(input.size() == 0) {
 			rec.add_score(-rec.score / 2);
 		} else if(input[0] == 'n') {
 			rec.add_score(1000 * 10);
 		} else if(input[0] == 'a') {
-			
+			rec.add_score(-rec.score / 8 * 7);
+		} else if(input[0] == 's') {
 		}
 		cout << endl;
 	}
